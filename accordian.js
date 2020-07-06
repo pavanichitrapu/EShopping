cf0be3c942abdb3d0ac75dfb917db4c58bee6f9d
pavanichitrapu <63398525+pavanichitrapu@users.noreply.github.com>
2020-07-06 16:53:06 +0530

    Add files via upload

---
+++ accordian.js
@@ -0,0 +1,11 @@
+var hamburger=document.getElementById('ham');
+var accord=document.getElementById('accordian');
+var sidebar=document.getElementById('sidebar');
+//sidebar.appendChild(accord);
+hamburger.addEventListener('click',function(){
+    accord.style.display="block";
+  });
+var closeBtn=document.getElementById('cls-btn');
+closeBtn.addEventListener('click',function(){
+   accord.style.display="none";
+});
